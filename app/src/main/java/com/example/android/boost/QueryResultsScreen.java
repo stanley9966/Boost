b82a14c9be2641b7446d43789448940261c609fe
stanley9966 <stanley9966@hotmail.com>
2018-01-06 00:05:35 -0500

    Tried making the circle thing... saving cause trying something new

--- app/src/main/java/com/example/android/boost/QueryResultsScreen.java
+++ app/src/main/java/com/example/android/boost/QueryResultsScreen.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
 import android.view.MenuItem;
 
 import java.util.ArrayList;
@@ -16,12 +17,16 @@ import java.util.Vector;
 
 public class QueryResultsScreen extends AppCompatActivity {
 
+    private WinRateCircle winRateCircle;
+
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
     private ArrayList<Long> mMatchArrayList;
     private HashMap<Long, Boolean> mGameIdsAndWinLossMap;
+
+    private int screenWidth;
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -39,6 +44,8 @@ public class QueryResultsScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_query_results_screen);
+
+        winRateCircle = findViewById(R.id.winRateCircleId);
 
         // setting up RecyclerView
         mRecyclerView = findViewById(R.id.recycler_view);
